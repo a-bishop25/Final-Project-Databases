c2a53267239b3680958f04e05fb8d10114ba0422
a-bishop25 <143771789+a-bishop25@users.noreply.github.com>
2025-12-10 21:21:53 -0500

    take away my credentials
    accidental

--- data/MuniBonds_dashboard.py
+++ data/MuniBonds_dashboard.py
@@ -9,10 +9,10 @@ import numpy as np
 # 1. DATABASE CONFIGURATION
 # ==============================================================================
 DB_USER = 'postgres'
-DB_PASS = 'Brindlewood9'
+DB_PASS = 'postgres'
 DB_HOST = 'localhost'
 DB_NAME = 'municipal_bonds'
-DB_PORT = '5432'
+DB_PORT = '5433'
 
 CONNECTION_STRING = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
